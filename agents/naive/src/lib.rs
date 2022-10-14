31cdb2437ce26c97af4890dade87ae82f9d4080a
Avi Revah <avraham.a.revah@gmail.com>
2022-10-14 14:14:40 -0500

    Get linear memory of wasm agent

--- agents/naive/src/lib.rs
+++ agents/naive/src/lib.rs
@@ -1,4 +1,4 @@
 #[no_mangle]
-pub extern "C" fn add_one(x: i32) -> i32 {
-    x + 1
+pub extern "C" fn evaluate() -> i32 {
+    0
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use std::fs;
-use wasmer::{imports, Instance, Module, Store, Value};
+use wasmer::{imports, Instance, Module, Store, MemoryView};
 
 fn main() {
-    let path = "target/wasm32-unknown-unknown/release/naive.wasm";
-    let wasm_bytes = fs::read(&path).unwrap();
+    let path = "target/wasm32-unknown-unknown/debug/naive.wasm";
+    let wasm_bytes = std::fs::read(&path).unwrap();
     let store = Store::default();
     let module = Module::new(&store, wasm_bytes).unwrap();
-    let import_object = imports! {};
-    let instance = Instance::new(&module, &import_object).unwrap();
-
-    let add_one = instance.exports.get_function("add_one").unwrap();
-    let result = add_one.call(&[Value::I32(42)]).unwrap();
+    let instance = Instance::new(&module, &imports! {}).unwrap();
+    let memory = instance.exports.get_memory("memory").unwrap();
+    let memory_view: MemoryView<u8> = memory.view();
+    println!("Memory View Size = {}", memory_view.len() / 65536);
+    let evaluate = instance.exports.get_function("evaluate").unwrap();
+    let result = evaluate.call(&[]).unwrap();
     println!("Result = {:?}", result[0]);
 }
 
