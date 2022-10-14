5ee265c3c6cf8fe396fc58521242da67e765ea99
Avi Revah <avraham.a.revah@gmail.com>
2022-10-14 01:02:30 -0500

    Basic add_one wasm program

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::fs;
+use wasmer::{imports, Instance, Module, Store, Value};
+
+fn main() {
+    let path = "agents/naive/target/wasm32-unknown-unknown/release/naive.wasm";
+    let wasm_bytes = fs::read(&path).unwrap();
+    let store = Store::default();
+    let module = Module::new(&store, wasm_bytes).unwrap();
+    let import_object = imports! {};
+    let instance = Instance::new(&module, &import_object).unwrap();
+
+    let add_one = instance.exports.get_function("add_one").unwrap();
+    let result = add_one.call(&[Value::I32(42)]).unwrap();
+    println!("Result = {:?}", result[0]);
+}
+
